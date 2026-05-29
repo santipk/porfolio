c7cf96ad9ba213f3d5ebdf75dc6633269682056e
Santiago Abregu <76819506+santipk@users.noreply.github.com>
2026-05-28 23:43:26 -0300

    Remove debug console logs

--- src/main.js
+++ src/main.js
@@ -2,9 +2,4 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './styles/main.css'
 
-console.log('[PORTFOLIO] main.js loaded')
-console.log('[PORTFOLIO] Vue app mounting...')
-
 createApp(App).mount('#app')
-
-console.log('[PORTFOLIO] Vue app mounted successfully')
